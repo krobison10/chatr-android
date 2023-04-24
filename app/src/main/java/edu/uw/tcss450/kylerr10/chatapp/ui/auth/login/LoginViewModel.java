1fe959854f4965a0c22f1839830422bc6a3b4485
jaspersn <jasper.s.newkirk@gmail.comgit>
2023-04-24 15:31:34 -0700

    Added placeholder views to login fragment

---
+++ app/src/main/java/edu/uw/tcss450/kylerr10/chatapp/ui/auth/login/LoginViewModel.java
@@ -0,0 +1,15 @@
+package edu.uw.tcss450.kylerr10.chatapp.ui.auth.login;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
+/**
+ * A simple {@link ViewModel} subclass responsible for handling HTTP requests regarding user
+ * credentials.
+ * @author Jasper Newkirk
+ */
+public class LoginViewModel extends ViewModel {
+
+    public LoginViewModel() {
+        // TODO: Implement the ViewModel
+    }
+}

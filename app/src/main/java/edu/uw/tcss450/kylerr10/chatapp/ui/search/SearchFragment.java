21a20d1b6a69f0abb1a630e46bf6f520cb48b67c
krobison10 <kylerrobison10@outlook.com>
2023-04-28 22:57:51 -0700

    Blank search fragment added.

---
+++ app/src/main/java/edu/uw/tcss450/kylerr10/chatapp/ui/search/SearchFragment.java
@@ -0,0 +1,32 @@
+package edu.uw.tcss450.kylerr10.chatapp.ui.search;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.uw.tcss450.kylerr10.chatapp.R;
+
+/**
+ * create an instance of this fragment.
+ *
+ * @author Kyler Robison
+ */
+public class SearchFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}

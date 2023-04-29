4e837414b5ef08490d062fe17b9922219591ef1b
krobison10 <kylerrobison10@outlook.com>
2023-04-28 22:41:11 -0700

    Bottom nav between chat and home

--- app/src/main/java/edu/uw/tcss450/kylerr10/chatapp/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/kylerr10/chatapp/MainActivity.java
@@ -1,8 +1,14 @@
 package edu.uw.tcss450.kylerr10.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 /**
  * Main activity of the application.
@@ -11,9 +17,31 @@ import android.os.Bundle;
  */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Bottom navigation
+     */
+    private AppBarConfiguration mAppBarConfiguration;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeBottomNav();
+    }
+
+    /**
+     * Helper method to set up the bottom navigation bar.
+     */
+    private void initializeBottomNav() {
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_chat)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 }

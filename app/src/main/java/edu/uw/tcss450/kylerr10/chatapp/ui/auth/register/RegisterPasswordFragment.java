859d0b47eaf1f7b2f54519e4716bcbc78d68045a
krobison10 <kylerrobison10@outlook.com>
2023-05-08 13:42:54 -0700

    Password check icons update dynamically

--- app/src/main/java/edu/uw/tcss450/kylerr10/chatapp/ui/auth/register/RegisterPasswordFragment.java
+++ app/src/main/java/edu/uw/tcss450/kylerr10/chatapp/ui/auth/register/RegisterPasswordFragment.java
@@ -7,9 +7,13 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.regex.Pattern;
 
 import edu.uw.tcss450.kylerr10.chatapp.R;
 import edu.uw.tcss450.kylerr10.chatapp.databinding.FragmentRegisterPasswordBinding;
@@ -37,5 +41,70 @@ public class RegisterPasswordFragment extends Fragment {
                     .navigate(R.id.action_registerPasswordFragment_to_loginFragment);
 
         });
+
+        // Start out with register button disabled
+        mBinding.buttonRegister.setEnabled(false);
+
+        // Text watcher for password input listeners
+        TextWatcher textWatcher = new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                mBinding.buttonRegister.setEnabled(checkPasswords());
+            }
+        };
+
+        mBinding.editPassword.addTextChangedListener(textWatcher);
+        mBinding.editPasswordreenter.addTextChangedListener(textWatcher);
+
+    }
+
+    /**
+     * Helper method to set states of password requirement icons to be called upon changes
+     * in the values of either password input. Doubles as a method to check if the password
+     * requirements are met.
+     *
+     * @return true if all the password requirements are met
+     */
+    private boolean checkPasswords() {
+        String passText = mBinding.editPassword.getText().toString();
+        String passReenterText = mBinding.editPasswordreenter.getText().toString();
+
+        final int green = R.drawable.ic_check_circle_green_24dp;
+        final int grey = R.drawable.ic_check_circle_grey_24dp;
+
+        //Set length icon
+        boolean sufficientLength = passText.length() >= 8;
+        mBinding.iconPassLength.setImageResource(sufficientLength ? green : grey);
+
+        //Set lowercase icon
+        boolean containsLowercase = Pattern.compile("[a-z]").matcher(passText).find();
+        mBinding.iconPassLowercase.setImageResource(containsLowercase ? green : grey);
+
+        //Set uppercase icon
+        boolean containsUppercase = Pattern.compile("[A-Z]").matcher(passText).find();
+        mBinding.iconPassUppercase.setImageResource(containsUppercase ? green : grey);
+
+        //Set number icon
+        boolean containsNumber = Pattern.compile("\\d").matcher(passText).find();
+        mBinding.iconPassNumber.setImageResource(containsNumber ? green : grey);
+
+        //Set special character icon
+        boolean containsSpecial =
+                Pattern.compile("[()|¬¦!£$%^&*<>;#~_\\-+=@]").matcher(passText).find();
+        mBinding.iconPassSpecial.setImageResource(containsSpecial ? green : grey);
+
+        //Set match icon
+        boolean passwordsMatch = passText.equals(passReenterText);
+        mBinding.iconPassMatch.setImageResource(passwordsMatch ? green : grey);
+
+        return sufficientLength && containsLowercase && containsUppercase && containsNumber
+                && containsSpecial && passwordsMatch;
     }
 }

cc981d50263f3ba3a426fc7b39e061f5b0158a93
Bettyabera <bettyabe@uw.edu>
2023-05-25 15:09:38 -0700

    Home Fragment

---
+++ app/src/main/java/edu/uw/tcss450/kylerr10/chatapp/ui/home/HomeViewModel.java
@@ -0,0 +1,101 @@
+package edu.uw.tcss450.kylerr10.chatapp.ui.home;
+
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.auth0.android.jwt.JWT;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.uw.tcss450.kylerr10.chatapp.databinding.FragmentHomeBinding;
+import edu.uw.tcss450.kylerr10.chatapp.model.UserInfoViewModel;
+
+/**
+ * @author Betelhem
+ * ViewModel class for the HomeFragment handles fetching user data from the server.
+ */
+
+public class HomeViewModel extends AndroidViewModel {
+
+    private MutableLiveData<JSONObject> mResponse;
+
+    public HomeViewModel(@NonNull Application application) {
+        super(application);
+        mResponse = new MutableLiveData<>();
+    }
+
+    public MutableLiveData<JSONObject> getResponse() {
+        return mResponse;
+    }
+    /**
+     * Fetches user data from the server and updates the UI.
+     *
+     * @param userInfoViewModel The UserInfoViewModel instance for accessing the JWT token.
+     * @param binding           The FragmentHomeBinding instance for accessing UI views.
+     */
+    public void fetchUserData(UserInfoViewModel userInfoViewModel, FragmentHomeBinding binding) {
+        String url = "http://10.0.2.2:5000/user";
+        JWT jwtToken = userInfoViewModel.getJWT();
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Parse the JSON response and extract user information
+                        try {
+                            boolean success = response.getBoolean("success");
+                            if (success) {
+                                String firstName = response.getString("firstName");
+                                String lastName = response.getString("lastName");
+                                String userName = response.getString("username");
+                                String greeting = "Hello, " + userName;
+
+                                // Update your UI with the greeting
+                                binding.textGreeting.setText(greeting);
+                            } else {
+                                // Handle error case where success is false
+                                String message = response.getString("Not success");
+                                Log.e("FETCH", "Error: " + message);
+                            }
+                        } catch (JSONException e) {
+                            Log.e("FETCH", "Error parsing JSON response: " + e.getMessage());
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle error case where the network request fails
+                        Log.e("FETCH", "Error fetching user data: " + error.getMessage());
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + jwtToken);
+                return headers;
+            }
+        };
+
+        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(
+                10_000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+        ));
+        Volley.newRequestQueue(getApplication().getApplicationContext())
+                .add(jsonObjectRequest);
+    }
+}
